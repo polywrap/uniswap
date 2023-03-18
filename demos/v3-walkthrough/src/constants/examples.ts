b0a25e8dbfa7e3ac0bbb3ab47ac51e099fe1879f
dOrgJelli <jelli@dorg.tech>
2023-03-18 02:25:32 -0400

    todo list

--- demos/v3-walkthrough/src/constants/examples.ts
+++ demos/v3-walkthrough/src/constants/examples.ts
@@ -36,4 +36,31 @@ export const examples: Example[] = [
       fee: "MEDIUM"
     }
   },
+  {
+    name: "Get Pool Address",
+    description: "Each Uniswap V3 Pool is uniquely identified by 3 characteristics: token-in, token-out, and fee.",
+    uri: uniswapV3Uri,
+    method: "getPoolAddress",
+    args: {
+      tokenA: {
+        chainId: "MAINNET",
+        address: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+        currency: {
+          decimals: 18,
+          symbol: "WETH",
+          name: "Wrapped Ether"
+        }
+      },
+      tokenB: {
+        chainId: "MAINNET",
+        address: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+        currency: {
+          decimals: 6,
+          symbol: "USDC",
+          name: "USDC"
+        }
+      },
+      fee: "MEDIUM"
+    }
+  },
 ]
