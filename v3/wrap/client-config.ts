aeea8d63f8a8a052550e9631712a0920717d1017
Pileks <jure@amber-it.co>
2023-08-21 14:11:45 +0200

    update CLI and Client versions, bump minor version in wrapscan deployment, update CD to include PR title trigger

--- v3/wrap/client-config.ts
+++ v3/wrap/client-config.ts
@@ -3,14 +3,14 @@ import {
   Connection,
   Connections,
 } from "@polywrap/ethereum-provider-js";
-import { IClientConfigBuilder } from "@polywrap/client-config-builder-js";
+import { IWrapPackage, ClientConfigBuilder } from "@polywrap/client-js";
 import { Wallet } from "ethers";
 
-export function configure(builder: IClientConfigBuilder): IClientConfigBuilder {
+export function configure(builder: ClientConfigBuilder): ClientConfigBuilder {
   const defaultSigner = new Wallet(
     "0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d"
   );
-  return builder.addDefaults().addPackage(
+  return builder.addDefaults().setPackage(
     "wrap://ens/wraps.eth:ethereum-provider@2.0.0",
     ethereumProviderPlugin({
       connections: new Connections({
@@ -22,6 +22,6 @@ export function configure(builder: IClientConfigBuilder): IClientConfigBuilder {
         },
         defaultNetwork: "mainnet",
       }),
-    })
+    }) as IWrapPackage
   );
 }
